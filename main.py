4696c0b34c1a4bf6cc708010eb4c5bddf0ce9700
Brandon Tallman <bgtall96@gmail.com>
2025-03-25 08:59:03 -0700

    built break walls recursive function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from window import Window
 from cell import Cell
+from maze import Maze
 
 
 
@@ -7,14 +8,9 @@ from cell import Cell
 
 def main():
     win = Window(800, 600)
-    cell = Cell(win)
-    cell.has_bottom_wall = False
-    cell1 = Cell(win)
-    cell1.has_top_wall = False
-    cell.draw(50, 50, 100, 100)
-    cell1.draw(200, 100, 300, 300)
+    
+    maze = Maze(50, 50, 12, 16, 20, 20, win)
 
-    cell.draw_move(cell1, undo=True)
     win.wait_for_close()
 
 
--- maze.py
+++ maze.py
@@ -1,8 +1,10 @@
 from cell import Cell
 import time
+import random
+
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win, seed=None):
         self._x1 = x1
         self._y1 = y1
         self.num_rows = num_rows
@@ -11,8 +13,13 @@ class Maze:
         self.cell_size_y = cell_size_y
         self._win = win
         self._cells = []
+        if seed:
+            random.seed(seed)
+
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
+        
 
     def _create_cells(self):
         for i in range(self.num_cols):
@@ -44,4 +51,53 @@ class Maze:
         self._draw_cell(0, 0)
         exit.has_bottom_wall = False
         self._draw_cell(self.num_cols - 1, self.num_rows - 1)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            left = None
+            right = None
+            top = None
+            bottom = None
+            if i - 1 >= 0:
+                left = i - 1
+            if i + 1 < self.num_cols:
+                right = i + 1
+            if j - 1 >= 0:
+                top = j - 1
+            if j + 1 < self.num_rows:
+                bottom = j + 1
+            if left is not None and self._cells[left][j].visited is False:
+                to_visit.append((left, j))
+            if right is not None and self._cells[right][j].visited is False:
+                to_visit.append((right, j))
+            if top is not None and self._cells[i][top].visited is False:
+                to_visit.append((i, top))
+            if bottom is not None and self._cells[i][bottom].visited is False:
+                to_visit.append((i, bottom))
+
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+            
+            random_direction = random.randrange(len(to_visit))
+            new_i = to_visit[random_direction][0]
+            new_j = to_visit[random_direction][1]
+            if i == new_i:
+                if j + 1 == new_j:
+                    self._cells[i][j].has_bottom_wall = False
+                    self._cells[i][new_j].has_top_wall = False
+                else:
+                    self._cells[i][j].has_top_wall = False
+                    self._cells[i][new_j].has_bottom_wall = False
+            if j == new_j:
+                if i + 1 == new_i:
+                    self._cells[i][j].has_right_wall = False
+                    self._cells[new_i][j].has_left_wall = False
+                else:
+                    self._cells[i][j].has_left_wall = False
+                    self._cells[new_i][new_j].has_right_wall = False
+            self._break_walls_r(new_i, new_j)
+
 
